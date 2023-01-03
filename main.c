bdecc879cdc200c8ed02e2ce69bf7b87bdd060ba
Nasir1224 <nasirafridi78@gmail.com>
2023-01-03 10:52:35 +0500

    basic interface

--- main.c
+++ main.c
@@ -4,47 +4,56 @@
 int main()
 {
     int option;
-    printf("Select an option:\n");
-    printf("1. List of products\n");
-    printf("2. Record a sale\n");
-    printf("3. View histroy\n");
+
+    printf("Select an option : \n");
+    printf("1: Manage products\n");
+    printf("2: Record sale\n");
+    printf("3: View sale history\n");
     scanf("%d", &option);
-    printf("Select an option:\n1. List products\t\t2. Insert product\n");
-    scanf("%d", &option);
+
     if (option == 1)
     {
-        Product *list;
-        int size = read_products(&list);
-        int i;
-        for (i = 0; i < size; i++)
+        printf("Select an option : \n");
+        printf("1: List products\n");
+        printf("2: Find a product\n");
+        printf("3: Add product\n");
+        printf("4: Edit product\n");
+        printf("5: Delete product\n");
+        scanf("%d", &option);
+
+        if (option == 1)
         {
-            printf("Id: %d\n", list[i].id);
-            printf("Name: %s\n", list[i].name);
-            printf("Manufacturer: %s\n", list[i].manufacturer);
-            printf("Price: %f\n", list[i].price);
-            printf("---------------------------------------------------\n");
-            printf("---------------------------------------------------\n");
+        }
+        else if (option == 2)
+        {
+        }
+        else if (option == 3)
+        {
+        }
+        else if (option == 3)
+        {
+        }
+        else if (option == 4)
+        {
+        }
+        else if (option == 5)
+        {
+        }
+        else
+        {
+            printf("Invalid option");
         }
     }
     else if (option == 2)
     {
-        Product product;
-        printf("Enter id: ");
-        scanf("%d", &product.id);
-        printf("Enter name: ");
-        scanf("%s", &product.name);
-        printf("Enter manufacturer: ");
-        scanf("%s", &product.manufacturer);
-        printf("Enter price: ");
-        scanf("%f", &product.price);
-        if (!insert_product(product))
-        {
-            printf("Product inserted successfully.\n");
-        }        
     }
-    else 
+    else if (option == 3)
     {
-        printf("Invalid option selected.\n");
     }
+    else
+    {
+        printf("Invalid option");
+    }
+
     return 0;
 }
