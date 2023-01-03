20833467b995e9068cf7d616ba0a1e9dc1c8a19c
Mohammad Raza <raza7861664@gmail.com>
2023-01-03 23:37:38 +0500

    Added find, update & delete functions for products

--- products.c
+++ products.c
@@ -3,38 +3,109 @@
 
 const char *filename = "products.dat";
 
-int readProducts(Product **list)
+int readProducts(Product **list, int *len)
 {
     int file_size;
     FILE *fp = fopen(filename, "rb");
     if (!fp)
     {
-        printf("Error in opening file %s.\n", filename);
-        return 0;
+        return -1;
     }
     fseek(fp, 0L, SEEK_END);
     file_size = ftell(fp);
     fseek(fp, 0L, SEEK_SET);
-    *list = (Product*) malloc(file_size);
+    *len = file_size / sizeof(Product);
+    *list = (Product *)calloc(*len, sizeof(Product));
     if (fread(*list, 1, file_size, fp) != file_size)
     {
-        printf("Cannot read whole file %s.\n", filename);
         fclose(fp);
-        return 0;
+        return -2;
     }
     fclose(fp);
-    return file_size / sizeof(Product);
+    return 0;
+}
+
+int findProduct(int id, Product *product)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (!fp)
+        return -1;
+    while (fread(product, sizeof(Product), 1, fp))
+    {
+        if (product->id == id)
+        {
+            fclose(fp);
+            return 0;
+        }
+    }
+    fclose(fp);
+    return -3;
 }
 
 int insertProduct(Product product)
 {
-    FILE *fp = fopen(filename, "ab");
+    // Check if product already exists
+    FILE *fp;
+    Product temp;
+    if (findProduct(product.id, &temp) == 0)
+        return -4;
+
+    // Insert the product
+    fp = fopen(filename, "ab");
     if (!fp)
-    {
-        printf("Error in opening file %s.\n", filename);
         return -1;
-    }
     fwrite(&product, sizeof(product), 1, fp);
     fclose(fp);
     return 0;
+}
+
+int updateProduct(Product product)
+{
+    Product temp;
+    FILE *fp = fopen(filename, "rb+");
+    if (!fp)
+        return -1;
+    while (fread(&temp, sizeof(Product), 1, fp))
+    {
+        if (temp.id == product.id)
+        {
+            fseek(fp, -1 * sizeof(Product), SEEK_CUR);
+            fwrite(&product, sizeof(Product), 1, fp);
+            fclose(fp);
+            return 0;
+        }
+    }
+    fclose(fp);
+    return -3;
+}
+
+int deleteProduct(int id)
+{
+    Product *list;
+    int len, ret, i;
+    FILE *fp;
+
+    ret = readProducts(&list, &len);
+    if (ret)
+    {
+        return ret;
+    }
+
+    fp = fopen(filename, "wb");
+    if (!fp)
+    {
+        free(list);
+        return -1;
+    }
+    ret = -3;
+    for (i = 0; i < len; i++)
+    {
+        if (list[i].id == id)
+            ret = 0;
+        else
+            fwrite(&list[i], sizeof(Product), 1, fp);
+    }
+    free(list);
+    fclose(fp);
+    return ret;
 }
--- products.h
+++ products.h
@@ -4,7 +4,11 @@ typedef struct
     char name[100];
     char manufacturer[100];
     float price;
+    int stock;
 } Product;
 
 int readProducts(Product **list);
+int findProduct(int id, Product *product);
 int insertProduct(Product product);
+int updateProduct(Product product);
+int deleteProduct(int id);
